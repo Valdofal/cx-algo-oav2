6b861bb518904801fc32e175c741fd6acd369a95
valdofal <v.dofal@gmail.com>
2020-10-19 09:53:25 +0200

    e02.js

---
+++ e02.js
@@ -0,0 +1,19 @@
+const array = [10, 1,  6, 19, 18, 9, 12,13, 7, 16,  5,  8, 0, 14,3, 4, 11, 15, 17,]
+
+function insertionSort(arr){
+    let j = 0;
+    let temp;
+    for(let i = 0; i<arr.length; i ++){
+        temp = arr[i]
+        j = i
+        while(j > 0 && arr[j-1] > temp){
+            arr[j] = temp
+            j--
+            console.log(`${arr}\n`)
+        }
+        
+        }
+    return arr
+}
+
+console.log(insertionSort(array))
